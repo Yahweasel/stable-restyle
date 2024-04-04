6e40c900a29246a8d08f4d8264212c33e9a60199
Yahweasel <git-rpo@yahweasel.com>
2024-04-04 14:39:54 -0400

    An experimental new technique: binary search restyling

--- video-restyle.js
+++ video-restyle.js
@@ -20,6 +20,8 @@ const fs = require("fs/promises");
 const genImg = require("./generate-img");
 const sr = require("./stable-restyle");
 
+let framesPerSlide = 2;
+
 /**
  * Does this file exist?
  */
@@ -78,19 +80,64 @@ async function restyle(promptFile, in1, in2, out) {
     return sr.restyle(promptFile, in1, in2, out);
 }
 
-async function main() {
-    let framesPerSlide = 4;
-    let slidesPerKey = 4;
+/**
+ * Restyle this by binary combination from lo to hi.
+ */
+function binaryRestyle(meta, lo, hi) {
+    if (hi <= lo + 1)
+        return;
 
+    let mid = ~~((hi + lo) / 2);
+    while ((mid%framesPerSlide) != 0)
+        mid++;
+    if (mid >= hi)
+        mid -= framesPerSlide;
+    if (mid <= lo)
+        return;
+
+    meta.promises[mid] = meta.locks[mid].then(async () => {
+        meta.unlocks[lo]();
+        meta.unlocks[hi]();
+        await meta.promises[lo];
+        await meta.promises[hi];
+
+        const loSlide = ~~(lo / framesPerSlide) + 1;
+        const midSlide = ~~(mid / framesPerSlide) + 1;
+        const hiSlide = ~~(hi / framesPerSlide) + 1;
+
+        // Motion-interpolate up
+        await interpolateMotion(
+            lo+1, mid+1, 1,
+            `out/${six(loSlide)}.png`,
+            `interp/${six(midSlide)}-f.png`
+        );
+
+        // And down
+        await interpolateMotion(
+            hi+1, mid+1, -1,
+            `out/${six(hiSlide)}.png`,
+            `interp/${six(midSlide)}-b.png`
+        );
+
+        // And restyle
+        await restyle(
+            "claymation14.json",
+            `interp/${six(midSlide)}-f.png`, `interp/${six(midSlide)}-b.png`,
+            `out/${six(midSlide)}.png`
+        );
+    });
+
+    // Subdivide
+    binaryRestyle(meta, lo, mid);
+    binaryRestyle(meta, mid, hi);
+}
+
+async function main() {
     for (let ai = 2; ai < process.argv.length; ai++) {
         const arg = process.argv[ai];
         switch (arg) {
             case "--fps":
                 framesPerSlide = +process.argv[++ai];
-                break;
-
-            case "--spk":
-                slidesPerKey = +process.argv[++ai];
                 break;
 
             default:
@@ -99,129 +146,65 @@ async function main() {
         }
     }
 
-    const framesPerKey = framesPerSlide * slidesPerKey;
-
     await run(["mkdir", "-p", "interp"]);
     await run(["mkdir", "-p", "out"]);
 
-    // First frame is a direct translation
-    console.log(1);
-    await restyle(
-        "claymation14.json",
-        "in/000001.png", "in/000001.png",
-        "out/000001.png"
-    );
-
-    let outSlide = 1;
-
-    const promises = [];
-
-    // One key at a time...
-    for (let keyNum = 1 + framesPerKey;; keyNum += framesPerKey) {
-        if (!(await exists(`in/${six(keyNum)}.png`)))
+    // Count frames
+    let frameCt;
+    for (frameCt = 1;; frameCt++) {
+        if (!(await exists(`in/${six(frameCt)}.png`))) {
+            frameCt--;
             break;
-
-        // Interpolate the motion forward
-        await interpolateMotion(
-            keyNum - framesPerKey, keyNum, 1,
-            `out/${six(outSlide)}.png`,
-            `interp/${six(outSlide + slidesPerKey)}.png`
-        );
-        outSlide += slidesPerKey;
-        const outSlideL = outSlide;
-
-        // Regenerate the final frame with AI
-        console.log(outSlideL);
-        const finalFramePromise = restyle(
-            "claymation14.json",
-            `in/${six(keyNum)}.png`, `interp/${six(outSlideL)}.png`,
-            `out/${six(outSlideL)}.png`
-        );
-
-        // Regenerate the first half with forward AI
-        const forwardPromise = (async () => {
-            for (let i = 1; i < slidesPerKey/2; i++) {
-                const inSlide = keyNum - framesPerKey + i*framesPerSlide;
-                const out = outSlideL - slidesPerKey + i;
-                await interpolateMotion(
-                    inSlide - framesPerSlide, inSlide, 1,
-                    `out/${six(out-1)}.png`,
-                    `interp/${six(out)}.png`
-                );
-                console.log(out);
-                await restyle(
-                    "claymation16.json",
-                    `in/${six(inSlide)}.png`, `interp/${six(out)}.png`,
-                    `out/${six(out)}.png`
-                );
-            }
-        })();
-
-        // Regenerate the second half with backward AI
-        const backwardPromise = finalFramePromise.then(async () => {
-            for (let i = 1; i < slidesPerKey/2; i++) {
-                const inSlide = keyNum - i*framesPerSlide;
-                const out = outSlideL - i;
-                await interpolateMotion(
-                    inSlide + framesPerSlide, inSlide, -1,
-                    `out/${six(out+1)}.png`,
-                    `interp/${six(out)}.png`
-                );
-                console.log(out);
-                await restyle(
-                    "claymation16.json",
-                    `in/${six(inSlide)}.png`, `interp/${six(out)}.png`,
-                    `out/${six(out)}.png`
-                );
-            }
-        });
-
-        // If there's a middle frame, regenerate that from both
-        let middlePromise = Promise.all([]);
-        if (slidesPerKey%2 === 0) middlePromise = middlePromise.then(async () => {
-            await forwardPromise;
-            await backwardPromise;
-
-            const i = slidesPerKey/2;
-            const inSlide = keyNum - i*framesPerSlide;
-            const out = outSlideL - i;
-            await interpolateMotion(
-                inSlide - framesPerSlide, inSlide, 1,
-                `out/${six(out-1)}.png`,
-                `interp/${six(out)}-f.png`
-            );
-            await interpolateMotion(
-                inSlide + framesPerSlide, inSlide, -1,
-                `out/${six(out+1)}.png`,
-                `interp/${six(out)}-b.png`
-            );
-            if (!await exists(`interp/${six(out)}.png`)) {
-                await run([
-                    "ffmpeg", "-loglevel", "error",
-                    "-i", `interp/${six(out)}-f.png`,
-                    "-i", `interp/${six(out)}-b.png`,
-                    "-filter_complex", "[0:v][1:v]blend=all_expr=A*0.5+B*0.5[vid]",
-                    "-map", "[vid]",
-                    "-y", "-update", "1",
-                    `interp/${six(out)}.png`
-                ]);
-            }
-            console.log(out);
-            await restyle(
-                "claymation16.json",
-                `in/${six(inSlide)}.png`, `interp/${six(out)}.png`,
-                `out/${six(out)}.png`
-            );
-        });
-
-        await finalFramePromise;
-        promises.push(Promise.all([
-            forwardPromise, backwardPromise, middlePromise
-        ]));
-        if (promises.length > 2)
-            await promises.shift();
+        }
     }
-    await Promise.all(promises);
+    frameCt = ~~(frameCt / framesPerSlide) * framesPerSlide;
+
+    // Create all the frame locks
+    const unlocks = Array(frameCt).fill(null);
+    const locks = Array(frameCt).fill(null).map((x, i) => new Promise(res => unlocks[i] = res));
+    console.log(locks);
+    const promises = Array(frameCt).fill(null);
+    const meta = {unlocks, locks, promises};
+
+    // First frame is a direct translation
+    promises[0] = locks[0].then(async () => {
+        await restyle(
+            "claymation14.json",
+            "in/000001.png", "in/000001.png",
+            "out/000001.png"
+        );
+    });
+
+    const groupSize = ~~(1024 / framesPerSlide) * framesPerSlide;
+
+    // Binary-restyle in groups of (usually) 1024
+    let idx;
+    for (idx = groupSize; idx < frameCt - 1; idx += groupSize) {
+        promises[idx] = locks[idx].then(async () => {
+            await restyle(
+                "claymation14.json",
+                `in/${six(idx+1)}.png`, `in/${six(idx+1)}.png`,
+                `out/${six(idx/framesPerSlide+1)}.png`
+            );
+        });
+        binaryRestyle(meta, idx - groupSize, idx);
+    }
+
+    // And the last group
+    promises[frameCt-1] = locks[frameCt-1].then(async () => {
+        await restyle(
+            "claymation14.json",
+            `in/${six(frameCt)}.png`, `in/${six(frameCt)}`,
+            `out/${six(frameCt/framesPerSlide)}.out`
+        );
+    });
+    binaryRestyle(meta, idx, frameCt-1);
+
+    // Then wait for promises
+    for (idx = 0; idx < frameCt; idx++) {
+        unlocks[idx]();
+        await promises[idx];
+    }
 }
 
 main();
